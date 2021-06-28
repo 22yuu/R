c691cf8077db5b209289523e27b3ea4bf5629784
22yuu <22yuu@naver.com>
2021-06-28 15:43:44 +0900

    2021/06/28 R 기본 문법 변수 선언, 벡터, 팩터

---
+++ Ch01/1_2_DataType.R
@@ -0,0 +1,32 @@
+# 날짜 : 2021/06/28
+# 이름 : 이진유
+# 내용 : ch01. R설치와 개요 - 변수 교재 p41
+
+# 숫자형
+num1 <- 1
+num2 <- 2
+num3 <- 3.14
+num1
+num2;num3
+
+# 논리형
+bool1 <- TRUE
+bool2 <- FALSE
+bool1;bool2
+
+
+# 문자형
+str1 <- "Hello"
+str2 <- 'Welcome'
+str1
+str2
+
+
+# 특수형
+data1 <- NULL  # 초기값을 의미
+data2 <- NA    # 결측값을 의미, Not Available
+data3 <- NaN   # 숫자가 아닌 값, Not a Number
+
+data1
+data2
+data3
